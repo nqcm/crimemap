9b9456604a03423a6865177b4f913377b6f562a5
Naveera <ndhedhi@gmail.com>
2017-08-17 13:24:30 +0200

    Skeleton CrimeMap

---
+++ dbhelper.py
@@ -0,0 +1,39 @@
+import pymysql
+import dbconfig
+
+class DBHelper:
+    def connect(self, database="crimemap"):
+        return pymysql.connect(host='localhost',
+            user=dbconfig.db_user,
+            passwd=dbconfig.db_password,
+            db=database)
+
+    def get_all_inputs(self):
+        connection = self.connect()
+        try:
+            query = "SELECT description FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+            return cursor.fetchall()
+        finally:
+            connection.close()
+
+    def add_input(self, data):
+        connection = self.connect()
+        try:
+            query = "INSERT INTO crimes (description) VALUES ('{}');".format(data)
+            with connection.cursor() as cursor:
+                connection.execute(query)
+                connection.commit()
+        finally:
+            connection.close()
+
+    def clear_all(self):
+        connection = self.connect()
+        try:
+            query = "DELET FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+            finally:
+                connection.close()
