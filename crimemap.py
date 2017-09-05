ed35efec79e9a0d122d9203dbbf325f769aebc3e
Naveera <ndhedhi@gmail.com>
2017-09-05 16:05:30 +0200

    user data sanitizing

--- crimemap.py
+++ crimemap.py
@@ -5,6 +5,9 @@ from flask import render_template
 from flask import request
 import dbconfig
 import json
+import datetime
+import dateparser
+import string
 
 
 if dbconfig.test:
@@ -12,14 +15,16 @@ if dbconfig.test:
 else:
     from dbhelper import DBHelper
 
+categories = ['mugging', 'break-in']
+
 app = Flask(__name__)
 DB = DBHelper()
 
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes, categories=categories, error_message=error_message)
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -41,14 +46,30 @@ def clear():
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return home()
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
 
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + " !?$.,;:-'()&'"
+    return filter(lambda x: x in whitelist, userinput)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
